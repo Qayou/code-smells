9d4f5f7d03bc4928361e2db49b21af5c27aa18cb
Pierre Charpentier <charpentier.pierre@outlook.com>
2020-04-24 21:58:46 +0200

    Long Method code smell

---
+++ Bloaters/LongMethod/dirty.ts
@@ -0,0 +1,16 @@
+const dirtyComputePrice = (): number => {
+  const items = Array(10)
+    .fill(0)
+    .map(() => ({ price: Math.random() * 10 }));
+
+  const itemsPriceSum = items.reduce(
+    (priceSum, item) => priceSum + item.price,
+    0
+  );
+
+  const finalPrice = itemsPriceSum * 0.8;
+
+  return finalPrice;
+};
+
+console.log(dirtyComputePrice());
